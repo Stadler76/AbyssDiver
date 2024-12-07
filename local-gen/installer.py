7aff842fe935fc5a37c6d76ec304bc92b65dc7dc
SPOOK_EXE <91944990+SPOOKEXE@users.noreply.github.com>
2024-12-07 11:32:19 +0930

    custom-install-cause-other-runs-it

--- local-gen/installer.py
+++ local-gen/installer.py
@@ -269,9 +269,77 @@ def comfy_ui_experimental_amd_windows(storage_directory : str) -> None:
 
 	print("Dependencies have been installed.")
 
-	setup_batch = Path(os.path.join(comfyui_directory, "install.bat")).as_posix()
+	custom_install_script : str = r"""@echo off
+title ComfyUI-Zluda Installer
+
+setlocal EnableDelayedExpansion
+set "startTime=%time: =0%"
+
+cls
+echo -------------------------------------------------------------
+Echo ******************* COMFYUI-ZLUDA INSTALL *******************
+echo -------------------------------------------------------------
+echo.
+echo  ::  %time:~0,8%  ::  - Setting up the virtual enviroment
+Set "VIRTUAL_ENV=venv"
+If Not Exist "%VIRTUAL_ENV%\Scripts\activate.bat" (
+    python.exe -m venv %VIRTUAL_ENV%
+)
+
+If Not Exist "%VIRTUAL_ENV%\Scripts\activate.bat" Exit /B 1
+
+echo  ::  %time:~0,8%  ::  - Virtual enviroment activation
+Call "%VIRTUAL_ENV%\Scripts\activate.bat"
+echo  ::  %time:~0,8%  ::  - Updating the pip package 
+python.exe -m pip install --upgrade pip --quiet
+echo.
+echo  ::  %time:~0,8%  ::  Beginning installation ...
+echo.
+echo  ::  %time:~0,8%  ::  - Installing required packages
+pip install -r requirements.txt --quiet
+echo  ::  %time:~0,8%  ::  - Installing torch for AMD GPUs (First file is 2.7 GB, please be patient)
+pip uninstall torch torchvision torchaudio -y --quiet
+pip install torch==2.3.0 torchvision==0.18.0 torchaudio==2.3.0 --index-url https://download.pytorch.org/whl/cu118 --quiet
+echo  ::  %time:~0,8%  ::  - Installing onnxruntime (required by some nodes)
+pip install onnxruntime --quiet
+echo  ::  %time:~0,8%  ::  - (temporary numpy fix)
+pip uninstall numpy -y --quiet
+pip install numpy==1.26.0 --quiet
+echo.
+echo  ::  %time:~0,8%  ::  Custom node(s) installation ...
+echo. 
+echo  ::  %time:~0,8%  ::  - Installing Comfyui Manager
+cd custom_nodes
+git clone https://github.com/ltdrdata/ComfyUI-Manager.git --quiet
+echo  ::  %time:~0,8%  ::  - Installing ComfyUI-deepcache
+git clone https://github.com/styler00dollar/ComfyUI-deepcache.git --quiet
+cd ..
+echo. 
+echo  ::  %time:~0,8%  ::  - Patching ZLUDA (Zluda 3.8.4 for HIP SDK 5.7)
+curl -s -L https://github.com/lshqqytiger/ZLUDA/releases/download/rel.c0804ca624963aab420cb418412b1c7fbae3454b/ZLUDA-windows-rocm5-amd64.zip > zluda.zip
+tar -xf zluda.zip
+del zluda.zip
+copy zluda\cublas.dll venv\Lib\site-packages\torch\lib\cublas64_11.dll /y >NUL
+copy zluda\cusparse.dll venv\Lib\site-packages\torch\lib\cusparse64_11.dll /y >NUL
+copy zluda\nvrtc.dll venv\Lib\site-packages\torch\lib\nvrtc64_112_0.dll /y >NUL
+@echo  ::  %time:~0,8%  ::  - ZLUDA is patched.
+echo. 
+set "endTime=%time: =0%"
+set "end=!endTime:%time:~8,1%=%%100)*100+1!"  &  set "start=!startTime:%time:~8,1%=%%100)*100+1!"
+set /A "elap=((((10!end:%time:~2,1%=%%100)*60+1!%%100)-((((10!start:%time:~2,1%=%%100)*60+1!%%100), elap-=(elap>>31)*24*60*60*100"
+set /A "cc=elap%%100+100,elap/=100,ss=elap%%60+100,elap/=60,mm=elap%%60+100,hh=elap/60+100"
+echo ..................................................... 
+echo *** Installation is completed in %hh:~1%%time:~2,1%%mm:~1%%time:~2,1%%ss:~1%%time:~8,1%%cc:~1% . 
+echo *** You can use "comfyui.bat" to start the app later. 
+echo ..................................................... 
+"""
+
+	setup_batch = Path(os.path.join(comfyui_directory, "custom_install.bat")).as_posix()
+	with open(setup_batch, 'w') as file:
+		file.write(custom_install_script)
+
 	if os.path.exists(setup_batch):
-		print("Running install.bat")
+		print("Running custom_install.bat")
 		print(setup_batch)
 		subprocess.run([setup_batch], check=True, cwd=comfyui_directory)
 
@@ -325,7 +393,9 @@ pause""")
 	thread1.join()
 	thread2.join()
 	thread3.join()
+
 	print("Both ComfyUI and Proxy scripts have finished.")
+	print('NOTICE: The first generation may take a bit, however, all generations after should be faster.')
 
 def download_checkpoints_to_subfolder(models_folder : str) -> None:
 	"""Download the checkpoints to the sub-folder checkpoints"""
